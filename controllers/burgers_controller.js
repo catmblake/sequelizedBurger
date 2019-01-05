c093b1d0e4c144b53e6e9885bb535dabe082a0ef
catmblake <catmblake@gmail.com>
2019-01-04 19:41:22 -0800

    adding validation to models

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,10 @@
 var express = require("express");
+var db = require("./../models");
 var router = express.Router();
 
-// Import the burgers.js model to use its database functions.
-var burger = require("../models/burger.js");
-
 // Create all routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
+router.get("/", function (req, res) {
+  db.Burger.findAll({}).then(function (data) {
     var hbsObject = {
       burgers: data
     };
@@ -14,30 +12,28 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.insertOne([
-    "burger_name"
-  ], [
-    req.body.burger_name
-  ], function(result) {
-    // Send back the ID of the new burger
-    res.json({ id: result.insertId });
+router.post("/api/burgers", function (req, res) {
+  db.Burger.create(req.body).then(function (data) {
+    res.json(data);
   });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = `id=${req.params.id}`;
-
-  burger.updateOne({
+router.put("/api/burgers/:id", function (req, res) {
+  db.Burger.update({
     devoured: req.body.devoured
-  }, condition, function(result) {
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then(function(data){
     if (result.changedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
     }
+  }) 
   });
-});
+
 // Export routes for server.js to use.
 module.exports = router;
